bb7109ef237ecf265aeea71f6fa4fd4abfe6d89f
Emil Berbakov <emil.berbakov@dciinc.org>
2024-11-04 18:17:07 -0600

    extras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
 import { RouterOutlet } from '@angular/router';
-
+import { DisabledFormFieldJustAlterAppearanceDirective } from './disabled-form-field-just-alter-appearance.directive';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+            RouterOutlet,
+            MatFormFieldModule,
+            MatInputModule,
+            MatSlideToggleModule,
+            MatSelectModule,
+            DisabledFormFieldJustAlterAppearanceDirective,
+            MatDatepickerModule,
+            MatNativeDateModule
+          ],
+  providers: [
+  {
+    provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+    useValue: {
+      appearance: 'outline',
+      subscriptSizing: 'dynamic',
+      floatLabel: 'always'
+      }
+  }
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'DisabledFormField';
+  options = [
+    { label: 'Option 1', value: 'Option 1' },
+    { label: 'Option 2', value: 'Option 2' },
+    { label: 'Option 3', value: 'Option 3' },
+    { label: 'Option 4', value: 'Option 4' },
+  ]
 }
---
+++ src/app/disabled-form-field-just-alter-appearance.directive.ts
@@ -0,0 +1,52 @@
+import { AfterViewInit, booleanAttribute, ChangeDetectorRef, contentChild, DestroyRef, Directive, effect, inject, input, signal, untracked, WritableSignal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatFormField, MatFormFieldAppearance, MatFormFieldControl } from '@angular/material/form-field';
+import { map, startWith } from 'rxjs/operators';
+
+@Directive({
+  selector: '[disabledAppearance]',
+  standalone: true,
+})
+export class DisabledFormFieldJustAlterAppearanceDirective implements AfterViewInit {
+
+  //disabledAppearance = input(false, { transform: booleanAttribute });
+  autoFloat = input(false, { transform: booleanAttribute })
+  placeHolderText = input('');
+  private matFormField = inject(MatFormField, { self: true });
+  cdr = inject(ChangeDetectorRef);
+  destroyRef = inject(DestroyRef);
+  formFieldInput = contentChild(MatFormFieldControl);
+  appearance: WritableSignal<MatFormFieldAppearance> = signal('fill');
+  originalPlaceholder = '';
+
+  constructor() {
+    effect(() => {
+      if (this.matFormField) {
+        this.matFormField.appearance = this.appearance();
+        this.matFormField.hideRequiredMarker = this.appearance() === 'fill'
+        const autoFloat = untracked(this.autoFloat);
+        const placeHolderText = untracked(this.placeHolderText);
+        if (autoFloat) {
+          this.matFormField.floatLabel = this.appearance() === 'fill' ? 'auto': 'always'
+        }
+        //Not Quite Working yet
+        if (placeHolderText) {
+          this.appearance() === 'fill' ?
+          this.formFieldInput()?.placeholder?.replace(this.formFieldInput()?.placeholder ?? '', placeHolderText):
+          this.formFieldInput()?.placeholder?.replace(this.formFieldInput()?.placeholder ?? '', this.originalPlaceholder)
+        }
+        this.cdr.detectChanges();
+      }
+    })
+  }
+
+  ngAfterViewInit(): void {
+
+    this.formFieldInput()!.stateChanges.pipe(startWith('This value does not matter as long as it is not null!'), map(() => this.formFieldInput()!.disabled ? 'fill' : 'outline'), takeUntilDestroyed(this.destroyRef))
+    .subscribe(appearance => {
+      this.appearance.set(appearance);
+      this.originalPlaceholder = this.formFieldInput()?.placeholder ?? ''
+      console.log(this.originalPlaceholder)
+    })
+  }
+}
